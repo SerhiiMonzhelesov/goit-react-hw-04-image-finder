b700283bd3cdc271d46a6cbb45aa01d6221a470b
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-07-19 17:51:51 +0300

    hook for SearchBar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,41 +1,39 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import StyledSearchBar from './StyledSearchBar';
 import { CiSearch } from 'react-icons/ci';
 import PropTypes from 'prop-types';
 
-class SearchBar extends Component {
-  state = { searchNameImg: '' };
+function SearchBar({ handleSubmit }) {
+  const [searchNameImg, setSearchNameImg] = useState('');
 
-  onSearchSubmit = event => {
+  const onSearchSubmit = event => {
     event.preventDefault();
-    this.props.handleSubmit(this.state.searchNameImg);
+    handleSubmit(searchNameImg);
   };
 
-  onInputChange = event => {
-    this.setState({ searchNameImg: event.target.value });
+  const onInputChange = event => {
+    setSearchNameImg(prevState => (prevState = event.target.value));
   };
 
-  render() {
-    return (
-      <StyledSearchBar className="searchbar">
-        <form className="form" onSubmit={this.onSearchSubmit}>
-          <button type="submit" className="button">
-            <CiSearch className="icon" />
-            <span className="button-label">Search</span>
-          </button>
+  return (
+    <StyledSearchBar className="searchbar">
+      <form className="form" onSubmit={onSearchSubmit}>
+        <button type="submit" className="button">
+          <CiSearch className="icon" />
+          <span className="button-label">Search</span>
+        </button>
 
-          <input
-            className="input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.onInputChange}
-          />
-        </form>
-      </StyledSearchBar>
-    );
-  }
+        <input
+          className="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={onInputChange}
+        />
+      </form>
+    </StyledSearchBar>
+  );
 }
 
 export default SearchBar;
